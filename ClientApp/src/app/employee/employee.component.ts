1a247970d7e5c1334df35b6dd885e6bda5838b24
LukasJCode <89071785+LukasJCode@users.noreply.github.com>
2021-09-28 12:55:42 +0200

    AngularV2 Wrap up

--- ClientApp/src/app/employee/employee.component.ts
+++ ClientApp/src/app/employee/employee.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IEmployee } from './employee';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { EmployeeService } from './employee.service';
+import { SubscriptionLike } from 'rxjs';
 
 @Component({
   selector: 'my-employee',
@@ -12,13 +13,33 @@ import { EmployeeService } from './employee.service';
 export class EmployeeComponent implements OnInit {
   employee: IEmployee;
   statusMessage: string = 'Loading data. Please wait...';
-  
-  constructor(private employeeService: EmployeeService, private activatedRoute: ActivatedRoute) {
+  subscription: SubscriptionLike;
+
+  constructor(private employeeService: EmployeeService, private activatedRoute: ActivatedRoute,
+    private router: Router) {
 
   }
+
+  //Promise
+  //ngOnInit(): void {
+  //  let empCode: string = this.activatedRoute.snapshot.params['code'];
+  //  this.employeeService.getEmployee(empCode).then((employeeData) => {
+  //    if (employeeData == null) {
+  //      this.statusMessage = 'Employee with given code does not exist';
+  //    }
+  //    else {
+  //      this.employee = employeeData
+  //    }
+  //  }).catch((error) => {
+  //      this.statusMessage = 'Problem with a service. Please try again later';
+  //      console.log(error);
+  //    });
+  //}
+
+  //Observable
   ngOnInit(): void {
     let empCode: string = this.activatedRoute.snapshot.params['code'];
-    this.employeeService.getEmployee(empCode).subscribe((employeeData) => {
+    this.subscription = this.employeeService.getEmployee(empCode).subscribe((employeeData) => {
       if (employeeData == null) {
         this.statusMessage = 'Employee with given code does not exist';
       }
@@ -29,5 +50,9 @@ export class EmployeeComponent implements OnInit {
         this.statusMessage = 'Problem with a service. Please try again later';
         console.log(error);
       });
-    }
+  }
+
+  onBackButtonClick(): void {
+    this.router.navigate(['/employees'])
+  }
 }
--- ClientApp/src/app/employee/employee.service.ts
+++ ClientApp/src/app/employee/employee.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { IEmployee } from './employee';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { map, retry, catchError } from "rxjs/operators";
+import { map, retry, catchError, retryWhen, delay, take } from "rxjs/operators";
 import { Observable, throwError } from 'rxjs';
 
 @Injectable()
@@ -11,9 +11,16 @@ export class EmployeeService {
 
   getEmployees(): Observable<IEmployee[]> {
     return this.http.get<IEmployee[]>("https://localhost:44382/Employees/GetEmployees")
-      .pipe(retry(1), catchError(this.handleError));
+      .pipe(retryWhen((error) => error.pipe(delay(1000), take(10))), catchError(this.handleError));
   }
 
+  //Promise
+  //getEmployee(code: string): Promise<IEmployee> {
+  //  return this.http.get<IEmployee>("https://localhost:44382/Employees/GetEmployee/" + code)
+  //    .pipe(retry(1), catchError(this.handleError)).toPromise()
+  //}
+
+  //Observable
   getEmployee(code: string): Observable<IEmployee> {
     return this.http.get<IEmployee>("https://localhost:44382/Employees/GetEmployee/" + code)
       .pipe(retry(1), catchError(this.handleError));
